ee38aa1dfce915925dee1f870d66b89f4b6efd48
EduardoDosSantosFerreira <eduardosferreira69@gmail.com>
2024-02-19 18:42:11 -0300

    update_v2

--- js/main.js
+++ js/main.js
@@ -12,3 +12,33 @@ previewButton.addEventListener("click", () => {
     }
     isPreviewing = !isPreviewing;
 });
+
+// categoria
+
+const categoriaLinks = document.querySelectorAll('.categoria-link');
+const projetos = document.querySelectorAll('#projetos > div');
+
+categoriaLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+        const categoria = link.getAttribute('data-categoria');
+
+        projetos.forEach(projeto => {
+            if (projeto.id === `projetos-${categoria}`) {
+                projeto.style.opacity = '0';
+                projeto.style.transition = 'opacity 0.5s';
+                setTimeout(() => {
+                    projeto.style.display = 'block';
+                    setTimeout(() => {
+                        projeto.style.opacity = '1';
+                    }, 50);
+                }, 500);
+            } else {
+                projeto.style.opacity = '0';
+                setTimeout(() => {
+                    projeto.style.display = 'none';
+                }, 500);
+            }
+        });
+    });
+});
