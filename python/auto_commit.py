b8b78cac89417879bb717139a55404a7878d414a
EduardoDosSantosFerreira <eduardosferreira69@gmail.com>
2025-02-16 01:08:44 -0300

    Commit automático

--- python/auto_commit.py
+++ python/auto_commit.py
@@ -1,22 +1,26 @@
 import os
 import subprocess
 
+def run_command(command):
+    result = subprocess.run(command, capture_output=True, text=True)
+    if result.returncode != 0:
+        print(f"Erro: {result.stderr}")
+
 def auto_commit():
     repo_path = "E:/web-sites/portfolio"
     os.chdir(repo_path)
 
     print("Atualizando repositório...")
-
-    subprocess.run(["git", "pull"], check=True)
+    run_command(["git", "pull"])
 
     print("Adicionando alterações...")
-    subprocess.run(["git", "add", "."], check=True)
+    run_command(["git", "add", "."])
 
     print("Criando commit...")
-    subprocess.run(["git", "commit", "-m", "Commit automático"], check=True)
+    run_command(["git", "commit", "-m", "Commit automático"])
 
     print("Enviando para o repositório remoto...")
-    subprocess.run(["git", "push"], check=True)
+    run_command(["git", "push"])
 
     print("Processo concluído.")
 
