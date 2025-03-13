a7bc61c847db055e445b2109cb14d92264b0ea24
EduardoDosSantosFerreira <eduardosferreira69@gmail.com>
2025-03-13 14:00:50 -0300

    update

--- src/assets/js/ftr.js
+++ src/assets/js/ftr.js
@@ -11,11 +11,11 @@ function adicionarLoginAoDOM() {
                     <hr class="bg-white">
                 </div>
                 <div class="col-md-12">
-                    <a href="http://" data-aos="fade-down"><img src="../assets/img/icons/wts.png" alt="Whatsapp" title="Whatsapp" id="ico-ftr"></a>
-                    <a href="http://" data-aos="fade-down"><img src="../assets/img/icons/in.png" alt="Linkedin" title="Linkedin" id="ico-ftr"></a>
-                    <a href="http://" data-aos="fade-down"><img src="../assets/img/icons/dc.png" alt="Discord" title="Discord" id="ico-ftr"></a>
-                    <a href="http://" data-aos="fade-down"><img src="../assets/img/icons/gmail.png" alt="Gmail" title="Gmail" id="ico-ftr"></a>
-                    <a href="http://" data-aos="fade-down"><img src="../assets/img/icons/github.png" alt="Github" title="Github" id="ico-ftr"></a>
+                    <a href="https://wa.me/5513981492795" data-aos="fade-down"><img src="../assets/img/icons/wts.png" alt="Whatsapp" title="Whatsapp" id="ico-ftr"></a>
+                    <a href="https://www.linkedin.com/in/eduardo-ferreira-1a7b2b1b9/" data-aos="fade-down"><img src="../assets/img/icons/in.png" alt="Linkedin" title="Linkedin" id="ico-ftr"></a>
+                    <a href="https://discord.com/users/eduardo_dsf" data-aos="fade-down"><img src="../assets/img/icons/dc.png" alt="Discord" title="Discord" id="ico-ftr"></a>
+                    <a href="mailto:eduardosferreira69@gmail.com" data-aos="fade-down"><img src="../assets/img/icons/gmail.png" alt="Gmail" title="Gmail" id="ico-ftr"></a>
+                    <a href="https://github.com/EduardoDosSantosFerreira" data-aos="fade-down"><img src="../assets/img/icons/github.png" alt="Github" title="Github" id="ico-ftr"></a>
                 </div>
             </div>
             <br>
