f1e9ccd60590da5ee24e564abf0f93dc05285e4f
EduardoDosSantosFerreira <eduardosferreira69@gmail.com>
2025-03-24 01:04:02 -0300

    Atualização automática: 2025-03-24 01:04:02

--- src/assets/js/components/navscript.js
+++ src/assets/js/components/navscript.js
@@ -8,9 +8,9 @@ function adicionarNavbarAoDOM() {
     <a href="../../../index.html">
         <img src="../assets/img/profile_icons/crowico.png" id="icon" alt="Logomarca EduardoDosSantosFerreira">
     </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+    <button class="navbar-toggler" type="button" id="toggler" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+        <span class="navbar-toggler-icon" id="toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
         <ul class="navbar-nav">
