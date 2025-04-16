92d8796b6e1b6decef843893fc2051752e394fff
EduardoDosSantosFerreira <eduardosferreira69@gmail.com>
2025-04-16 15:30:41 -0300

    Atualização automática: 2025-04-16 15:30:41

--- python/auto_commit.bat
+++ python/auto_commit.bat
@@ -1,3 +1,3 @@
 @echo off
-"C:\Users\Eduardo\AppData\Local\Programs\Python\Python313\python.exe" "E:\web-sites\portfolio\python\auto_commit.py"
+"C:\Users\Eduardo\AppData\Local\Programs\Python\Python313\python.exe" "F:\web-sites\portfolio\python\auto_commit.py"
 exit
